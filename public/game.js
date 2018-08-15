26047de234ecaf7d0c242bf7a6a6b69d0a6db7f8
zeanort <armandwork@hotmail.co.uk>
2018-08-15 14:55:48 +0100

    added user input time and validation

---
+++ public/game.js
@@ -0,0 +1,41 @@
+
+ var input = document.querySelector('#alphabet');
+ var reset = document.querySelector('#reset');
+
+
+ var startTime = 0; 
+ var endTime = 0; 
+ var alphabet = "abcdefghijlkmnopqrstuvwxyz";
+ var score = null;
+ 
+
+
+
+ input.addEventListener('keyup', function(e) {
+  
+    if (startTime == 0) {
+    startTime = new Date();
+  } else if (endTime == 0 && input.value == alphabet) {
+      endTime = new Date();
+      score = endTime - startTime;
+      console.log(score);
+  }
+  var matcher = new RegExp(input.value)
+//   console.log(matcher);
+ console.log( matcher.test(alphabet));
+ if (matcher.test(alphabet) === true) {
+     input.classList.remove('mistake');
+ } else {
+     input.classList.add('mistake');
+ }
+
+ });
+
+ reset.addEventListener('click', function(e){
+     e.preventDefault();
+     startTime = 0;
+     endTime = 0; 
+     score = null;
+     input.value = null;
+     input.focus();
+ })
