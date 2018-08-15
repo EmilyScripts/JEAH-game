ee12970fd45bc86001c7062b02e18fd673636625
zeanort <armandwork@hotmail.co.uk>
2018-08-15 16:24:48 +0100

    xhr request and data population on DOM

--- public/api.js
+++ public/api.js
@@ -1,10 +1,12 @@
+var table = document.querySelector('table')
+
 var dummy = [
   {name: 'Armand', score: 11.5},
   {name: 'Jessie', score: 3.2},
   {name: 'Harry', score: 7}
 ]
 
-function request (data, cb) {
+function request (cb) {
   var xhr = new XMLHttpRequest()
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
@@ -14,6 +16,27 @@ function request (data, cb) {
       console.log('There was an error getting the data', xhr.responseType)
     }
   }
-  xhr.open(GET, '/data', true)
+  xhr.open('GET', /data/, true)
   xhr.send()
 }
+
+function renderData (arr) {
+  var count = 1
+
+  arr.forEach(function (el) {
+    var row = document.createElement('tr')
+    appendTableData(row, count)
+    appendTableData(row, el.name)
+    appendTableData(row, el.score)
+    count++
+    table.appendChild(row)
+  })
+}
+
+function appendTableData (row, text) {
+  var td = document.createElement('td')
+  td.textContent = text
+  row.appendChild(td)
+}
+
+request(renderData)
