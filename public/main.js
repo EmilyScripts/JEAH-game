4cd9981e0a7015ee79d3259a9975ed217ffce65e
harryyy27 <harryyy27@gmail.com>
2018-08-16 12:26:19 +0100

    sign up modal

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 var modal = document.getElementById('modal')
 var login = document.getElementById('Login')
+var rModal = document.getElementById('registration-modal')
+var signUp = document.getElementById('sign-up')
 login.addEventListener('click', function (e) {
   e.preventDefault()
   modal.style.display = 'block'
@@ -7,6 +9,17 @@ login.addEventListener('click', function (e) {
 
 window.onclick = function (event) {
   if (event.target === modal) {
+    modal.style.display = 'none'
+  }
+}
+
+signUp.addEventListener('click', function (e) {
+  e.preventDefault()
+  rModal.style.display = 'block'
+})
+
+window.onclick = function (event) {
+  if (event.target === rModal) {
     modal.style.display = 'none'
   }
 }
